95ac49f5bb1c5c86b89296c5ae3f7cfb157d0f53
Ko-dst-ya <78589527+Ko-dst-ya@users.noreply.github.com>
2023-06-01 22:22:48 +0300

    Create extract_irreps.py

---
+++ extract_irreps.py
@@ -0,0 +1,30 @@
+def extract_irreps(file_irreps,selected_modes=set(),freq_range=(0,np.inf)):
+    '''
+        file_irreps -- name of irreps.yaml file
+        selected_modes -- set of labels to be extracted, e.g.: selected_modes={'Ag'}
+        freq_range -- tuple that stores minimum and maximum frequencies and the function will return only modes from this range
+    
+    return: a tuple of only necessary frequencies (in terms of labels) and corresponding labels
+    '''
+    with open(file_irreps, 'r') as f:
+        data_irreps = yaml.load(f, Loader=yaml.SafeLoader)
+        
+    modes = data_irreps['normal_modes']
+    
+    frequency = []
+    irrep = []
+    
+    # Gathering of all irreducible labels in selected_modes
+    if not selected_modes:
+        for mode in modes:
+            label = mode['ir_label']
+            selected_modes.add(label)
+    
+    for mode in modes:
+        freq = mode['frequency'] * conv_THz_to_inv_cm
+        label = mode['ir_label']
+        if label in selected_modes and freq > freq_range[0] and freq < freq_range[-1]:
+            frequency.append(freq)
+            irrep.append(label)
+        
+    return (frequency,irrep)

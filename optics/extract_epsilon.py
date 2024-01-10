f556d583520f96cce4ba5ca55c2b652cb9c4c468
Ko-dst-ya <78589527+Ko-dst-ya@users.noreply.github.com>
2024-01-10 17:02:43 +0300

    Update extract_epsilon.py
    Function boarders_for_epsilon(vasprun_file) is added.
    This finds starting and ending lines inside a given vasprun.xml file for real and imaginary parts of dielectic function togehter with separating boarders for 'density-density' and 'current-current' parts.

--- optics/extract_epsilon.py
+++ optics/extract_epsilon.py
@@ -1,3 +1,49 @@
+def boarders_for_epsilon(vasprun_file):
+    '''
+    Find boarders for density-density and current-current 
+    frequency dependent dielectric function
+    inside a given vasprun.xml file
+    Boarders mean the starting and ending number of lines
+    for frequency dependent epsilon. In other words
+    
+    The first line is followed by <set>:
+    
+    <set>
+    <r>     0.0000     0.0000     0.0000     0.0000     0.0000     0.0000     0.0000 </r>
+    
+    The </set> is followed by the last line:
+    
+    <r>    40.4672     0.0001     0.0001     0.0001    -0.0000     0.0000     0.0000 </r>
+     </set>
+
+    
+    vasprun_file - string name of vasprun.xml file
+    '''
+    boarders = {'real': {'dd':[0,0],
+                         'cc':[0,0]},
+                'imag': {'dd':[0,0],
+                         'cc':[0,0]}}
+    
+    count = 0
+
+    with open(vasprun_file,'r') as f:
+        for line in f:
+            count +=1 
+
+            if 'density-density' in line:
+                kind_of_approximation = 'dd'
+            elif 'current-current' in line:
+                kind_of_approximation = 'cc'
+
+            for part_of_eps in ['real','imag']:
+                if f'<{part_of_eps}>' in line:
+                    boarders[part_of_eps][kind_of_approximation][0] = count+10
+                elif f'</{part_of_eps}>' in line:
+                    boarders[part_of_eps][kind_of_approximation][-1] = count-2
+    
+    return boarders
+
+
 def read_epsilon(file):
   '''
   file -- vasprun.xml file containing real and imaginary parts of epsilon of hv
